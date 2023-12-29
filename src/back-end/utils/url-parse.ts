a8f66f701b1469053b0dee6ebaf33024bc68666b
Amani Mavu <amanimavu@gmail.com>
2023-12-29 19:09:04 +0300

    modify the regex for extracting id from URL

--- src/back-end/utils/url-parse.ts
+++ src/back-end/utils/url-parse.ts
@@ -7,7 +7,7 @@
  * @returns {number| null}
  */
 export const getIdFromUrl = (url: string): number | null => {
-    const regExp = /(?<=\/)d+$/;
+    const regExp = /(?<=\/)\d+$/;
     const match = regExp.exec(url);
     return match && parseInt(match[0], 10);
 };
