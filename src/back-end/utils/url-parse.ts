100055cd10893fda8c692cfad8b39be4c46400d8
Amani Mavu <amanimavu@gmail.com>
2023-12-29 11:57:06 +0300

    create a function to extract filters from url

--- src/back-end/utils/url-parse.ts
+++ src/back-end/utils/url-parse.ts
@@ -3,3 +3,14 @@ export const getIdFromUrl = (url: string): number | null => {
     const match = regExp.exec(url);
     return match && parseInt(match[0], 10);
 };
+
+export const getFiltersFromUrl = (url: string): string[][] | null => {
+    const queryStringRegExp = /(?<=\?).+$/;
+    const queryParamPairsRegExp = /[a-z]+=\w+/g;
+
+    const queryString = queryStringRegExp.exec(url);
+    const queryParamPairs =
+        queryString && queryString[0].match(queryParamPairsRegExp);
+
+    return queryParamPairs && queryParamPairs.map((pair) => pair.split("="));
+};
